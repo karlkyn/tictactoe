6c4bf333183ce345495efc2a5eb9201c489f2e33
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-03 13:09:22 +0000

    update to deploy script

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -3,11 +3,10 @@
 set -exo pipefail
 
 echo "Connecting to deploy server"
-ssh -t -t root@178.62.64.70 '
-
-docker kill $(docker ps -q)
-docker rm `docker ps -a -q`
-docker pull aevarisak/tictactoe
-docker run -p 80:8080 -d -e "NODE_ENV=production" aevarisak/tictactoe
-'
+ssh -t -t root@178.62.64.70 bash -c "'
+docker kill $(docker ps -q);
+docker rm `docker ps -a -q`;
+docker pull aevarisak/tictactoe;
+docker run -p 80:8080 -d -e "NODE_ENV=production" aevarisak/tictactoe;
+'"
 echo "Done"
