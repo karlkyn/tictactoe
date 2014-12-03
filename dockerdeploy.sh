dcbc7ef5c711e5ec89f17b820b52b83c136338e0
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-03 23:37:23 +0000

    build and deploy test

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#set -exo pipefail
-
-#export PATH=$PATH:/usr/local/bin
-
-#echo "Connecting to deploy server"
-#ssh -o StrictHostKeyChecking=no -t -t root@178.62.64.70'
-#bash -s;
 sudo docker kill tictactoe
 sudo docker rm `docker ps -a -q`
 sudo docker pull aevarisak/tictactoe
 sudo docker run --name tictactoe -p 80:8080 -d -e "NODE_ENV=production" aevarisak/tictactoe
-#echo "Done"
+echo "Done"
