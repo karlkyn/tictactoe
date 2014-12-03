46e31167e9890b1de7f0674680ee6e0adcecad40
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-03 10:48:42 +0000

    Adding deploy script

---
+++ dockerdeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -exo pipefail
+
+echo "Connecting to deploy server"
+ssh root@178.62.64.70
+
+echo "Stopping Docker"
+docker kill $(docker ps -q)
+
+echo "Deleting old containers"
+docker rm `docker ps -a -q`
+
+echo "Pulling Docker repository"
+docker pull aevarisak/tictactoe
+
+echo "Running Docker image"
+docker run -p 80:8080 -d -e "NODE_ENV=production" aevarisak/tictactoe
+
+echo "Done"
