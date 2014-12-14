f853651c724523574bae8570422acae6be227937
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-14 12:57:04 +0000

    Tic tac toe context working

--- server/model/tictactoe/tictactoeContext.js
+++ server/model/tictactoe/tictactoeContext.js
@@ -1,3 +1,13 @@
-/**
- * Created by aevar on 14.12.14.
- */
+'use strict';
+
+var _ = require('lodash');
+
+module.exports = function(eventStore, cmdHandler) {
+  return {
+    handleCommand: function(cmd) {
+      var eventStream = eventStore.loadEvents(cmd.id);
+      var events = cmdHandler(eventStream).executeCommand(cmd);
+      return events;
+    }
+  }
+};
