57c1ac9ebf799421c6c93eb4052d590e5f5c9e16
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-16 23:51:50 +0000

    Join game api implemented and tested

--- server/api/joinGame/joinGame.spec.js
+++ server/api/joinGame/joinGame.spec.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var should  = require('should');
+var request = require('supertest');
+var app     = require('../../app');
+
+describe('POST /api/joinGame', function() {
+  it('should respond with json array', function(done) {
+    var cmd = {
+      id: '12345',
+      cmd: 'JoinGame',
+      user: {
+        username: 'karlkyn'
+      },
+      gameName: 'FunTimes',
+      timestamp: 1417851954
+    };
+
+    var req = request(app);
+
+    req.post('/api/joinGame')
+      .type('json')
+      .send(cmd)
+      .end(function(err, res) {
+        if (err) return done(err);
+        //Assert
+        res.body.should.be.instanceOf(Array);
+        done();
+      });
+  });
+});
