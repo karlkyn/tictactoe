c2ab22ba932d5b2e76f83e9eb93436a7c3e10bf3
Ævar Ísak Ástþórsson <aevarisak@gmail.com>
2014-12-16 23:48:55 +0000

    Place move api implemented and tested

--- server/api/placeMove/placeMove.spec.js
+++ server/api/placeMove/placeMove.spec.js
@@ -1,1 +1,35 @@
 'use strict';
+
+var should  = require('should');
+var request = require('supertest');
+var app     = require('../../app');
+
+describe('POST /api/placeMove', function() {
+  it('should respond with json array', function(done) {
+    var cmd = {
+      id: '12345',
+      cmd: 'PlaceMove',
+      user: {
+        username: 'karlkyn'
+      },
+      gameName: 'FunTimes',
+      timestamp: 1417851954,
+      move: {
+        coordinates: [0,0],
+        symbol: 1
+      }
+    };
+
+    var req = request(app);
+
+    req.post('/api/placeMove')
+      .type('json')
+      .send(cmd)
+      .end(function(err, res) {
+        if (err) return done(err);
+        //Assert
+        res.body.should.be.instanceOf(Array);
+        done();
+      });
+  });
+});
